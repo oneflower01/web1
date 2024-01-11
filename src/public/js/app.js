02673e69e64d2415c69ab76cb9fcf7d4675ab621
oneflower01 <50243008+oneflower01@users.noreply.github.com>
2024-01-11 20:33:40 +0900

    4
    4

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,42 @@
-const socket = new WebSocket(`ws://${window.location.host}`)
+const socket = io()
+
+const room = document.getElementById("room")
+const form = room.querySelector("form")
+
+socket.emit("enter_room", () =>{
+    console.log("server is done!")
+})
+
+
+function addMessage(message){
+    const ul = room.querySelector("ul")
+    const li = document.createElement("li")
+    li.innerText = message
+    ul.appendChild(li)
+}
+
+
+
+
+socket.on("welcome", () =>{
+    addMessage("someone joined!")
+})
+
+socket.on("new_message", (msg)=>{
+    addMessage(msg)
+})
+
+
+function sendMessage(event){
+    event.preventDefault()
+    const input = form.querySelector("input")
+    const value = input.value
+    socket.emit("new_message", value, ()=>{
+        addMessage(`You: ${value}`)
+    })
+    input.value = ""
+}
+
+
+
+form.addEventListener("submit", sendMessage)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http"
-import WebSocket, {WebSocketServer} from "ws"
+import {Server} from "socket.io";
 import express from 'express'
 import path from 'path'
 const __dirname = path.resolve()
@@ -7,27 +7,29 @@ const __dirname = path.resolve()
 
 const app = express()
 
-
-
 app.set("view engine", "pug")
 app.set("views", __dirname + "/src/views")
-console.log(__dirname)
-
 app.use("/public", express.static(__dirname + "/src/public"))
-
 app.get("/", (req, res) => res.render("home"))
 app.get("/*", (req, res) => res.redirect("/"))
 
 
-const handleListen = () => console.log("http://...3000")
-const server = http.createServer(app)
-const wss = new WebSocketServer({server})
+const httpServer = http.createServer(app)
+const wsServer = new Server(httpServer)
+const roomname = "신세경사단"
+
+wsServer.on("connection", (socket)=>{
+    socket.on("enter_room", (done)=>{
+        done()
+        socket.join(roomname)
+        socket.to(roomname).emit("welcome")
+    })
+    socket.on("new_message", (msg, done) => {
+        socket.to(roomname).emit("new_message", msg)
+        done()
+    })
+})
 
 
-function handleConnection(socket){
-    console.log(socket)
-}
-
-wss.on("connection", handleConnection)
-
-server.listen(3000, handleListen)
+const handleListen = () => console.log("Listening on http://...3000")
+httpServer.listen(3000, handleListen)
