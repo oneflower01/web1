acd2c28856fd8141f85486f900975f285d49e73b
oneflower01 <50243008+oneflower01@users.noreply.github.com>
2024-01-11 18:47:59 +0900

    1
    1

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import http from "http"
+import WebSocket from "ws"
+import  express from 'express'
+
+const app = express()
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => res.render("home"))
+app.get("/*", (req, res) => res.redirect("/"))
+
+
+const handleListen = () => console.log("http://...3000")
+const server = http.createServer(app)
+const wss = new WebSocket.Server({server})
+
+
+function handleConnection(socket){
+    console.log(socket)
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen)
